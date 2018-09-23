bf3beabe9e9167b3594cb2c2303f6909b49b47ee
plutasnyy <kamil.plucinski97@gmail.com>
2018-09-23 18:13:43 +0200

    created homepage

---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import NickNameForm from "./NickNameForm";
+import './../css/HomePage.css'
+
+const HomePage = () => (
+    <div className={'HomePage'}>
+        <NickNameForm/>
+    </div>
+)
+
+export default HomePage
